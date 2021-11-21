d2ed4aea960608e8ca8ed04637a2cec8be12ac74
Berke Can Goktas <berke200395@gmail.com>
2021-11-21 17:38:41 +0300

    MisraC++

---
+++ MISRAC++/1-DilHataları/Rule 1-1-6/Rule 1-1-6/Rule 1-1-6.cpp
@@ -0,0 +1,32 @@
+﻿// Rule 1-1-6.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int i = 10;
+int j = 8;
+
+
+int main()
+{
+    cout << "1 veya 0 giriniz:" << endl;
+    int x;
+    cin >> x;
+    int k = (i * i) + (3 * j);
+    if (x) {
+        cout << k; //Dikkat edilirse x 0 iken k ile hiçbir işlem yapılmaz bu nedenle k'nın tanımlaması if statementın içine alınmalıdır
+    }
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
---
+++ MISRAC++/1-DilHataları/Rule 1-2-1/Rule 1-2-1/Rule 1-2-1.cpp
@@ -0,0 +1,36 @@
+﻿// Rule 1-2-1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+struct s {
+    int m1[32];
+};
+
+struct t {
+    int m2;
+    struct s m3;
+};
+
+union {
+    struct s u1;
+    struct t u2;
+} a;
+
+int main()
+{
+    a.u2.m3 = a.u1; //eşitlemenin iki tarafı da aynı objeyi gösteriyor
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
